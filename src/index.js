3d07e0c5f305cbf60fc69684d8108f7a7fa5c2a7
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-12 13:23:02 -0500

    feat(messaging): better formatted errors and such

--- src/index.js
+++ src/index.js
@@ -29,41 +29,42 @@
  * @typedef {PackageExtras & BasicPackage} PackageJson
  *
  */
-import fs from 'fs/promises';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import fse from 'fs-extra';
 
 import { packageJson } from 'ember-apply';
 import { migrateAddon } from './addon.js';
 import { migrateTestApp } from './test-app.js';
+import { writeRootPackageJson } from './workspaces.js';
+import { error, info } from './log.js';
 
 /**
  * @param {string} workingDirectory - the directory `npx ember-apply` was invoked fromm
  */
 export default async function run(workingDirectory) {
-  let info = await analyze();
+  let analysis = await analyze();
 
-  if (info.isV2) {
+  if (analysis.isV2 || !analysis.isAddon) {
+    error('Package is either not an addon or is already V2');
+
     return;
   }
 
-  await writeRootPackageJson(info);
-  await migrateAddon(info);
-  await migrateTestApp(info);
+  await writeRootPackageJson(analysis);
+  await migrateAddon(analysis);
+  await migrateTestApp(analysis);
 
-  console.info(`
+  info(`
     🎉 Congratulations! Your addon is now formatted as a V2 addon!
 
     Next steps:
-     - run \`${info.packager} install\`
-     - run \`${info.packager} run lint:fix\` in each workspace
+     - run \`${analysis.packager} install\`
+     - run \`${analysis.packager} run lint:fix\` in each workspace
      - test your addon in ./test-app
      - push up a branch
      - merge
      - release
      - party 🥳
-`);
+  `);
 }
 
 /**
@@ -73,6 +74,7 @@ export default async function run(workingDirectory) {
  * @property {string} packageName
  * @property {'yarn' | 'npm' | 'pnpm'} packager
  * @property {PackageJson} packageInfo
+ * @property {boolean} isAddon
  * @property {boolean} isV2
  *
  * @returns {Promise<Info>}
@@ -84,7 +86,7 @@ async function analyze() {
 
   /** @type {PackageJson} */
   let packageInfo = pkgJson;
-  let name = packageInfo.name;
+  let name = packageInfo.name || '';
   let workspace = name.split('/')[1] ?? name;
 
   let isYarn = fse.existsSync('yarn.lock');
@@ -97,57 +99,7 @@ async function analyze() {
     packageInfo,
     workspace,
     packageName: name,
+    isAddon: Boolean(packageInfo['ember-addon']),
     isV2: packageInfo['ember-addon']?.version === 2,
   };
 }
-
-/**
- * @param {Info} info
- */
-async function writeRootPackageJson(info) {
-  let rootJson = workspacePackageJsonFrom(info);
-
-  // root package.json must not contain 'ember-cli',
-  // otherwise ember-cli doesn't work :(
-  await fs.writeFile('package.json', JSON.stringify(rootJson, null, 2));
-}
-
-/**
- * @param {Info} info
- */
-function workspacePackageJsonFrom(info) {
-  let { workspace, packageInfo: old, packager } = info;
-
-  /** @type {Partial<PackageJson>} */
-  let rootJson = {
-    private: true,
-    workspaces: [workspace, 'test-app'],
-    repository: old.repository,
-    license: old.license,
-    author: old.author,
-    scripts: {},
-  };
-
-  if (packager === 'yarn') {
-    rootJson.scripts = {
-      ...rootJson.scripts,
-      build: `yarn workspace ${workspace} build`,
-      prepare: `yarn build`,
-    };
-  }
-
-  if (old.volta) {
-    rootJson.volta = old.volta;
-  }
-
-  if (old.engines) {
-    rootJson.engines = old.engines;
-  }
-
-  return rootJson;
-}
-
-// @ts-ignore
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-run.path = __dirname;
