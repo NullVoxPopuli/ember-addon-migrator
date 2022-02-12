cbdf7bd9aec2a2eeea755046915f7b963c019555
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-12 15:37:52 -0500

    fix(addon): write new package.json to addon directory

--- src/test-app.js
+++ src/test-app.js
@@ -1,60 +1,97 @@
+/**
+ * @typedef {import('./index').Info} Info
+ */
 import path from 'path';
 import fs from 'fs/promises';
 import fse from 'fs-extra';
 import { execa } from 'execa';
 import { globby } from 'globby';
-import semver from 'semver';
 import { packageJson } from 'ember-apply';
 
 /**
- *
- * @param {import('./index').Info} info
+ * @param {Info} info
  */
 export async function migrateTestApp(info) {
-  let { packageName, packageInfo } = info;
+  await createTestApp(info);
+  await moveTests(info);
+  // TODO: update in-test imports to use the test-app name instead of "dummy"
 
-  let emberCli = 'latest';
-
-  if (packageInfo.devDependencies) {
-    let coerced = semver.coerce(packageInfo.devDependencies['ember-cli']);
-
-    if (coerced) {
-      emberCli = `${coerced}`;
-    }
-  }
-
-  await createTestApp(emberCli, packageName);
-  await moveTests();
-
-  await moveFilesToTestApp();
+  await moveFilesToTestApp(info);
+  await updateFilesWithinTestApp(info);
   await removeFiles();
 }
 
-async function moveFilesToTestApp() {
+/**
+ * @param {Info} info
+ */
+async function moveFilesToTestApp(info) {
+  let { testWorkspace } = info;
+
   // Move useful files to test app
-  await fse.move('ember-cli-build.js', 'test-app/ember-cli-build.js', { overwrite: true });
+  let toMove = [
+    'ember-cli-build.js',
+    'config/ember-try.js',
+    '.template-lintrc.js',
+    '.prettierrc.js',
+    '.prettierignore',
+    '.eslintrc.js',
+  ];
+
+  await Promise.allSettled([
+    ...toMove.map((filePath) =>
+      fse.move(filePath, `${testWorkspace}/${filePath}`, { overwrite: true })
+    ),
+  ]);
 }
 
 /**
- * @param {string} emberCli
- * @param {string} name
+ * @param {Info} info
  */
-async function createTestApp(emberCli, name) {
-  let testAppLocation = path.join(process.cwd(), 'test-app');
+async function updateFilesWithinTestApp(info) {
+  let { testWorkspace } = info;
+
+  // ember-cli-build: EmberAddon => EmberApp
+  // ember-cli-build: 'ember-addon' => 'ember-app'
+  await replaceIn(`${testWorkspace}/ember-cli-build.js`, 'EmberAddon', 'EmberApp');
+  await replaceIn(`${testWorkspace}/ember-cli-build.js`, '/ember-addon', '/ember-app');
+}
+
+/**
+ * @param {Info} info
+ */
+async function createTestApp(info) {
+  let { packageName, testWorkspace } = info;
+  let testAppLocation = path.join(process.cwd(), testWorkspace);
 
   await fs.mkdir(testAppLocation, { recursive: true });
   await execa('ember', ['init', '--skip-npm', '--skip-git', '--embroider'], {
     cwd: testAppLocation,
     preferLocal: true,
   });
-  await packageJson.addDevDependencies({ [name]: '*' }, testAppLocation);
+  await packageJson.addDevDependencies({ [packageName]: '*' }, testAppLocation);
 }
 
-async function moveTests() {
-  const paths = await globby(['tests/*']);
+/**
+ * @param {Info} info
+ */
+async function moveTests(info) {
+  let { testWorkspace, isTs } = info;
+
+  await fse.remove('tests/dummy');
+
+  const paths = await globby(['tests/**/*']);
 
   for (let filePath of paths) {
-    await fse.move(filePath, `test-app/${filePath}`, { overwrite: true });
+    await fse.move(filePath, `${testWorkspace}/${filePath}`, { overwrite: true });
+  }
+
+  if (isTs) {
+    let jsSetup = `${testWorkspace}/tests/test-helper.js`;
+    let tsSetup = `${testWorkspace}/tests/test-helper.ts`;
+
+    if ((await fse.pathExists(jsSetup)) && (await fse.pathExists(tsSetup))) {
+      fse.remove(jsSetup);
+    }
   }
 }
 
@@ -65,6 +102,10 @@ async function moveTests() {
 async function removeFiles() {
   let unneededPaths = [
     'app',
+    'vendor',
+    'tests',
+    'config',
+    '.watchmanconfig',
     '.ember-cli',
     'types',
     'tsconfig.json',
@@ -76,8 +117,21 @@ async function removeFiles() {
 
   let topLevelJs = await globby('*.js');
 
-  await Promise.all([
+  await Promise.allSettled([
     ...unneededPaths.map((filePath) => fse.remove(filePath)),
     ...topLevelJs.map((filePath) => fse.remove(filePath)),
   ]);
+}
+
+/**
+ * @param {string} filePath
+ * @param {string} toFind
+ * @param {string} replaceWith
+ */
+async function replaceIn(filePath, toFind, replaceWith) {
+  let buffer = await fs.readFile(filePath);
+  let asString = buffer.toString();
+  let replaced = asString.replaceAll(toFind, replaceWith);
+
+  await fs.writeFile(filePath, replaced);
 }
