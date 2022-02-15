dcc81e6e0af942b32490e8bd86a0234aaa5e9844
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-02-14 21:01:45 -0500

    chore(internal, tests): add ts fixtures

---
+++ tests/fixtures/base-ts-v1-addon/tests/unit/the-test.ts
@@ -0,0 +1,9 @@
+import {module, test} from 'qunit';
+
+import {two} from 'base-ts-v1-addon';
+
+module('Unit | the test', function () {
+  test('it works', function (assert) {
+    assert.strictEqual(two, 'two');
+  });
+});
