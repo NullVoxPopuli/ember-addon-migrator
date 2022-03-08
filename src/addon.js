d341d7a0abab8c0788496923a5d8189f0da760a4
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-03-08 14:35:47 -0500

    chore: guard against non-existent test-support folders

--- src/addon.js
+++ src/addon.js
@@ -28,7 +28,11 @@ export async function migrateAddon(info) {
   }
 
   await fse.move('addon', `${workspace}/src`);
-  await fse.move('addon-test-support', `${workspace}/src/test-support`);
+
+  if (await fse.pathExists('addon-test-support')) {
+    await fse.move('addon-test-support', `${workspace}/src/test-support`);
+  }
+
   await writeAddonPackageJson(info);
 
   if (isTs) {
