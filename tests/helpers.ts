9fe0e9cbc5ed12c38e5a20aa7958d94bb894c6c9
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-16 16:05:45 -0500

    Try to run tests less serially per iteration

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,11 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import os from 'node:os';
+import fs from 'node:fs/promises';
+import path, { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { execa } from 'execa';
-import fs from 'fs';
 import fse from 'fs-extra';
-import { dirname, join } from 'path';
-import { Project } from 'scenario-tester';
-import { fileURLToPath } from 'url';
 import { expect } from 'vitest';
+
+export interface Project {
+  rootPath: string;
+  addonPath: string;
+  testAppPath: string;
+}
 
 /**
   * NOTE: these tests *only* use pnpm, becausue npm and yarn are frail 
@@ -13,31 +20,27 @@ import { expect } from 'vitest';
   */
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const fixturesDir = join(__dirname, 'fixtures');
+const binPath = join(__dirname, '..', 'bin.js');
+
+export async function findFixtures(): Promise<string[]> {
+  return (await fs.readdir(fixturesDir, { withFileTypes: true }))
+    .filter(stat => stat.isDirectory())
+    .map(stat => stat.name);
+}
+
+async function createTmpDir(prefix) {
+  let prefixPath = path.join(os.tmpdir(), prefix);
+
+  let tmpDirPath = await fs.mkdtemp(prefixPath);
+
+  return tmpDirPath;
+}
 
 export async function addonFrom(fixture: string): Promise<Project> {
   let packagePath = join(__dirname, 'fixtures', fixture, 'package.json');
-  let sampleAddon: null | { path: string; destroy: () => Promise<void> } = null;
-
-  if (!fs.existsSync(packagePath)) {
-    let sample = join(__dirname, 'samples', fixture, 'input.json');
-
-    if (!fs.existsSync(sample)) {
-      throw new Error(`Unable to resolve fixture ${fixture}`);
-    }
-
-    let addonObject = fse.readJSONSync(sample);
-
-    sampleAddon = await restoreAddon(addonObject);
-
-    if (!sampleAddon) {
-      throw new Error(`Unable to restore sample addon: ${fixture}`);
-    }
-
-    packagePath = join(sampleAddon.path, 'package.json');
-  }
 
   let originalPackageJsonPath = require.resolve(packagePath);
-  const nodeModulesPath = join(dirname(originalPackageJsonPath), 'node_modules');
 
   // This TS is compiled to CJS, so require is fine
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -60,44 +63,36 @@ export async function addonFrom(fixture: string): Promise<Project> {
   return project;
 }
 
-export const binPath = join(__dirname, '..', 'bin.js');
 
-export async function migrate(project: Pick<Project, 'baseDir'>) {
-  let { stdout } = await execa('node', [binPath], { cwd: project.baseDir });
+export async function migrate(project: Project) {
+  let { stdout } = await execa('node', [binPath], { cwd: project.rootPath });
 
   expect(stdout).toMatch(`🎉 Congratulations! Your addon is now formatted as a V2 addon!`);
 }
 
-export async function install(project: Pick<Project, 'baseDir'>) {
+export async function install(project: Project) {
   await execa('pnpm', ['install'], {
-    cwd: project.baseDir,
-    preferLocal: true,
+    cwd: project.rootPath,
   });
 }
 
-export async function build(project: Pick<Project, 'baseDir' | 'name'>) {
-  let addonPath = join(project.baseDir, project.name);
-  let { stdout, exitCode } = await execa('pnpm', ['run', 'build'], { cwd: addonPath });
+export async function build(project: Project) {
+  let { stdout, exitCode } = await execa('pnpm', ['build'], { cwd: project.addonPath });
 
   // subset of full stdout
   // can't use snapshot testing due to time taken printed
   // to stdout
   console.debug(stdout);
   expect(exitCode).toEqual(0);
-  expect(stdout).toMatch(`$ concurrently 'npm:build:*'`);
-  expect(stdout).toMatch('[build:*js] > rollup -c ./rollup.config.js');
-  expect(stdout).toMatch('[build:*docs] > cp ../README.md ./README.md');
-  // Message from concurrently
-  expect(stdout).toMatch('npm run build:js exited with code 0');
 
-  let result = await execa('ls', { cwd: join(project.baseDir, project.name) });
+  let result = await execa('ls', { cwd: project.addonPath });
 
   expect(result.stdout).toMatch('dist');
 }
 
-export async function emberTest(project: Pick<Project, 'baseDir'>) {
+export async function emberTest(project: Project) {
   let { stdout, exitCode } = await execa('pnpm', ['run', 'ember', 'test'], {
-    cwd: join(project.baseDir, 'test-app'),
+    cwd: project.testAppPath,
   });
 
   // subset of full stdout
@@ -109,6 +104,14 @@ export async function emberTest(project: Pick<Project, 'baseDir'>) {
   expect(stdout).toMatch('# skip  0');
   expect(stdout).toMatch('# todo  0');
   expect(stdout).toMatch('# fail  0');
+}
+
+export async function lintAddon(project: Project) {
+  return await execa('pnpm', ['lint'], { cwd: project.addonPath });
+}
+
+export async function lintTestApp(project: Project) {
+  return await execa('pnpm', ['lint'], { cwd: project.testAppPath });
 }
 
 export async function verify(fixtureName: string) {
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,39 @@
-import { describe, test } from 'vitest';
+import { expect, describe, test, beforeAll } from 'vitest';
 
-import { verify, findFixtures } from './helpers.js';
+import { findFixtures, addonFrom, migrate, emberTest, type Project, lintAddon, lintTestApp, build } from './helpers.js';
 
 let fixtures = await findFixtures();
 
 describe('fixtures', () => {
   for (let fixtureName of fixtures) {
-    test(fixtureName, async () => {
-      await verify(fixtureName);
-    });
+    describe(fixtureName, () => {
+      let project: Project;
+
+      beforeAll(async () => {
+        project = await addonFrom(fixtureName);
+
+      });
+
+      test('migrate & build', async () => {
+        await migrate(project);
+        await build(project);
+      });
+
+      test.concurrent('lint addon', async () => {
+        let result = await lintAddon(project)
+        expect(result).toMatchObject({ exitCode: 0 });
+      });
+
+      test.concurrent('lint test-app', async () => {
+        let result = await lintTestApp(project);
+        expect(result).toMatchObject({ exitCode: 0 });
+      });
+
+      test.concurrent('tests pass', async () => {
+        let result = await emberTest(project);
+        expect(result).toMatchObject({ exitCode: 0 });
+      });
+    }) 
   }
 })
 
