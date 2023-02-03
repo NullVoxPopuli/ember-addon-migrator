6fb313487e90dc29acd028c9de37a149014bebc5
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-02-03 12:07:08 -0500

    Make the reset command also delete the node_modules directory

--- cli/src/git-reset.js
+++ cli/src/git-reset.js
@@ -1,17 +1,21 @@
 import { execa } from 'execa';
+import Listr from 'listr';
 
 export default async function reset() {
-  await execa('git', ['clean', '-f', '-d']);
-  await execa('git', ['checkout', '.']);
+  let tasks = new Listr([
+    {
+      title: 'git clean -f -d',
+      task: () => execa('git', ['clean', '-f', '-d']),
+    },
+    {
+      title: 'git checkout .',
+      task: () => execa('git', ['checkout', '.']),
+    },
+    {
+      title: 'rm -rf node_modules/',
+      task: () => execa('rm', ['-rf', 'node_modules']),
+    },
+  ]);
 
-  // When things get weird? unsure if this is needed still
-  // let { stdout: status } = await execa('git', ['status']);
-
-  // /**
-  //  * Is there a way to do this for non-English git?
-  //  */
-  // if (status.includes('Untracked files')) {
-  //   await execa('git', ['add', '.']);
-  //   await execa('git', ['reset', '--hard', 'HEAD']);
-  // }
+  await tasks.run();
 }
