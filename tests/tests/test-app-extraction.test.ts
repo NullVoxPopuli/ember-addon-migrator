0fe4c0f8c463845e8a601e4bac0b8378e1c70a68
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-10 12:20:44 -0500

    Rename the tests so that testNamePattern is easier to use for each fixture
    Adjust maxthreads

--- tests/tests/test-app-extraction.test.ts
+++ tests/tests/test-app-extraction.test.ts
@@ -15,68 +15,66 @@ import {
 
 let fixtures = await findFixtures();
 
-describe('extract-tests: fixtures', () => {
-  for (let fixtureName of fixtures) {
-    describe(fixtureName, () => {
-      let project: Project;
+for (let fixtureName of fixtures) {
+  describe(`extract-tests on fixture: ${fixtureName}`, () => {
+    let project: Project;
 
-      beforeAll(async () => {
-        project = await addonFrom(fixtureName);
-        await extractTests(project, ['--in-place']);
-        // For tests, since we aren't in a monorepo,
-        // make the rootDir a monorepo
-        // "extra-tests" does not create monorepos
-        await fse.writeFile(
-          path.join(project.rootPath, 'package.json'),
-          JSON.stringify({ name: 'root', private: true, version: '0.0.0' })
-        );
-        await fse.writeFile(
-          path.join(project.rootPath, 'pnpm-workspace.yaml'),
-          `packages:\n` + `- package\n` + `- test-app\n`
-        );
-        await packageJson.modify((json) => {
-          if (json.dependencies?.[fixtureName]) {
-            json.dependencies[fixtureName] = `workspace:*`;
-          }
+    beforeAll(async () => {
+      project = await addonFrom(fixtureName);
+      await extractTests(project, ['--in-place']);
+      // For tests, since we aren't in a monorepo,
+      // make the rootDir a monorepo
+      // "extra-tests" does not create monorepos
+      await fse.writeFile(
+        path.join(project.rootPath, 'package.json'),
+        JSON.stringify({ name: 'root', private: true, version: '0.0.0' })
+      );
+      await fse.writeFile(
+        path.join(project.rootPath, 'pnpm-workspace.yaml'),
+        `packages:\n` + `- package\n` + `- test-app\n`
+      );
+      await packageJson.modify((json) => {
+        if (json.dependencies?.[fixtureName]) {
+          json.dependencies[fixtureName] = `workspace:*`;
+        }
 
-          if (json.devDependencies?.[fixtureName]) {
-            json.devDependencies[fixtureName] = `workspace:*`;
-          }
-        });
-
-        await install(project);
+        if (json.devDependencies?.[fixtureName]) {
+          json.devDependencies[fixtureName] = `workspace:*`;
+        }
       });
 
-      test.concurrent('verify tmp project', async () => {
-        await execa('ls', ['-la'], { cwd: project.rootPath, stdio: 'inherit' });
-
-        expect(
-          await fse.pathExists(project.rootPath),
-          `rootPath: ${project.rootPath}`
-        ).toBe(true);
-        expect(
-          await fse.pathExists(project.addonPath),
-          `addonPath: ${project.addonPath}`
-        ).toBe(false);
-        expect(
-          await fse.pathExists(project.packagePath),
-          `packagePath: ${project.packagePath}`
-        ).toBe(true);
-        expect(
-          await fse.pathExists(project.testAppPath),
-          `testAppPath: ${project.testAppPath}`
-        ).toBe(true);
-      });
-
-      test.concurrent('lint test-app', async () => {
-        let result = await lintTestApp(project);
-
-        expect(result).toMatchObject({ exitCode: 0 });
-      });
-
-      test.concurrent('tests pass', async () => {
-        await assertEmberTest(project);
-      });
+      await install(project);
     });
-  }
-});
+
+    test.concurrent('verify tmp project', async () => {
+      await execa('ls', ['-la'], { cwd: project.rootPath, stdio: 'inherit' });
+
+      expect(
+        await fse.pathExists(project.rootPath),
+        `rootPath: ${project.rootPath}`
+      ).toBe(true);
+      expect(
+        await fse.pathExists(project.addonPath),
+        `addonPath: ${project.addonPath}`
+      ).toBe(false);
+      expect(
+        await fse.pathExists(project.packagePath),
+        `packagePath: ${project.packagePath}`
+      ).toBe(true);
+      expect(
+        await fse.pathExists(project.testAppPath),
+        `testAppPath: ${project.testAppPath}`
+      ).toBe(true);
+    });
+
+    test.concurrent('lint test-app', async () => {
+      let result = await lintTestApp(project);
+
+      expect(result).toMatchObject({ exitCode: 0 });
+    });
+
+    test.concurrent('tests pass', async () => {
+      await assertEmberTest(project);
+    });
+  });
+}
--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
 // Vitest doesn't let this be actual node ESM
 // This is the fake TS-Node, but still actually CJS
+import os from 'node:os';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     root: __dirname,
+    maxThreads: Math.ceil(os.cpus().length / 2),
     testTimeout: 380_000,
     hookTimeout: 380_000,
     // https://vitest.dev/config/#configuration
